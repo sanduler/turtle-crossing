a72312782d0a0a2e63713f0b3689c469656dccbb
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-04 10:05:37 -0800

    added random start for one car

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("White")
+        self.shape("square")
+        self.shapesize(stretch_wid=2, stretch_len=1)
+        self.left(90)
+        self.random_start()
+        self.x_move = -3
+        self.y_move = 0
+        self.move_speed = 0.03
+
+    def movement(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def random_start(self):
+        self.goto(0, random.randrange(-280, 280))
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 player = Player()
+cars = CarManager()
 # turtle = Turtle()
 # turtle.penup()
 # turtle.shape("turtle")
@@ -21,13 +22,14 @@ player = Player()
 #     turtle.forward(30)
 
 # Car object
-car = Turtle()
-car.penup()
-car.color("White")
-car.shape("square")
-car.shapesize(stretch_wid=2, stretch_len=1)
-car.left(90)
-car.goto(250, 0)
+# car = Turtle()
+# car.penup()
+# car.color("White")
+# car.shape("square")
+# car.shapesize(stretch_wid=2, stretch_len=1)
+# car.left(90)
+# car.goto(250, 0)
+
 screen.listen()
 screen.onkey(player.go_up, "Up")
 
@@ -35,6 +37,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
+    cars.movement()
     if player.ycor() > 280:
         player.start_pos()
