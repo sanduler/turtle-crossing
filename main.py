7aa858ee91431cc74b893f07ea312b6c17c20cf8
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-04 09:24:56 -0800

    refractorred the code into the player class

--- main.py
+++ main.py
@@ -7,21 +7,20 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-turtle = Turtle()
-turtle.penup()
-turtle.shape("turtle")
-turtle.heading()
-turtle.left(90)
-turtle.goto(0, -280)
+player = Player()
+# turtle = Turtle()
+# turtle.penup()
+# turtle.shape("turtle")
+# turtle.heading()
+# turtle.left(90)
+# turtle.goto(0, -280)
 
 
-def go_up():
-    turtle.forward(30)
-
-screen.onkey(go_up, "Up")
+# def go_up():
+#     turtle.forward(30)
 
 screen.listen()
-
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
