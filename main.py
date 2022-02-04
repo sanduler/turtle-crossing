4a0779f4b5eacd5ed0e0a2853186edefbb35df67
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-04 09:16:50 -0800

    generated the turtle to cross the road

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-turtle = Turtle("Black")
+turtle = Turtle()
+turtle.penup()
+turtle.shape("turtle")
+turtle.heading()
+turtle.left(90)
+turtle.goto(0, -280)
+
+
+def go_up():
+    turtle.forward(30)
+
+screen.onkey(go_up, "Up")
+
+screen.listen()
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
